8f6ec8d2b5c144cca9c6a36967fc522a7178bb6a
Eddie Zaneski <eddiezane@gmail.com>
2012-08-22 19:20:15 -0400

    Add places array

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
 
+places = ["Baja Fresh Mexican Grill", "Qdoba Mexican Grill", "El Limon", 
+          "Chipotle Mexican Grill", "Machismo Burrito Bar"]
+
 get '/' do
   "Hello World!"
 end
