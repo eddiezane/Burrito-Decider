0438a91a57eb27d773da7b09d141fc9a7876dbec
Eddie Zaneski <eddiezane@gmail.com>
2012-08-23 12:25:24 -0400

    Clean views

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 require 'slim'
 
+configure do
+  set :public_folder, Proc.new { File.join(root, "static") }
+end
+
 get '/' do
   @places = ["Baja Fresh Mexican Grill", "Qdoba Mexican Grill", "El Limon", 
           "Chipotle Mexican Grill", "Machismo Burrito Bar"]
