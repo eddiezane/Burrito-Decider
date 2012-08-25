bc02746bb479dcc01e9b8ad7aff1a4de55677c29
Eddie Zaneski <eddiezane@gmail.com>
2012-08-24 22:20:23 -0400

    Add burritofinder class

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 require 'slim'
+require 'foursquare2'
+
+class BurritoFinder
+  def self.find_burrito(zip)
+    client.search_venues(:near => zip, :radius => '8000', :query => 'burrito', :verified => true).groups.first.items.sample
+  end
+
+  def self.client
+    @client ||= Foursquare2::Client.new(:client_id => ENV['FOURSQUARE_ID'], 
+                                        :client_secret => ENV['FOURSQUARE_SECRET'])
+  end
+end
 
 configure do
   set :public_folder, Proc.new { File.join(root, "static") }
